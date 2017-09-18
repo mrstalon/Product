d68af6d10b40db6632a88f7fc0569ffc88f97079
mrstalon <stalonadsl948@gmail.com>
2017-09-18 13:39:09 +0300

    done vol 2

--- src/index.js
+++ src/index.js
@@ -1,25 +1,24 @@
 module.exports = function multiply(first, second) {
     // your solution
 
-        let firstParsedNumber = first.split("").reverse(),
-            secondParsedNumber = second.toString().split("").reverse(),
-            ResultOfMultiply = [];
+    let firstParsedNumber = first.split("").reverse(),
+        secondParsedNumber = second.toString().split("").reverse(),
+        ResultOfMultiply = [];
 
-        for (let firstNumIteration = 0; firstNumIteration < firstParsedNumber.length; firstNumIteration++) {
-            for (let secondNumIteration = 0; secondNumIteration < secondParsedNumber.length; secondNumIteration++) {
-                let arrayPos = firstNumIteration + secondNumIteration;
+    for (let firstNumIteration = 0; firstNumIteration < firstParsedNumber.length; firstNumIteration++) {
+        for (let secondNumIteration = 0; secondNumIteration < secondParsedNumber.length; secondNumIteration++) {
+            let arrayPos = firstNumIteration + secondNumIteration;
 
-                ResultOfMultiply[arrayPos] = firstParsedNumber[firstNumIteration] * secondParsedNumber[secondNumIteration] + (ResultOfMultiply[arrayPos] === undefined ? 0 : ResultOfMultiply[arrayPos]);
+            ResultOfMultiply[arrayPos] = firstParsedNumber[firstNumIteration] * secondParsedNumber[secondNumIteration] + (ResultOfMultiply[arrayPos] === undefined ? 0 : ResultOfMultiply[arrayPos]);
 
-                if (ResultOfMultiply[arrayPos] > 9) {
-                    ResultOfMultiply[arrayPos + 1] = Math.floor(ResultOfMultiply[arrayPos] / 10)  + (ResultOfMultiply[arrayPos + 1] === undefined ? 0 : ResultOfMultiply[arrayPos + 1] );
-                    console.log("before : " + ResultOfMultiply[arrayPos]);
-                    ResultOfMultiply[arrayPos] -= Math.floor(ResultOfMultiply[arrayPos] / 10) * 10;
-                    console.log("after : " + ResultOfMultiply[arrayPos]);
-                }
+            if (ResultOfMultiply[arrayPos] > 9) {
+                ResultOfMultiply[arrayPos + 1] = Math.floor(ResultOfMultiply[arrayPos] / 10) + (ResultOfMultiply[arrayPos + 1] === undefined ? 0 : ResultOfMultiply[arrayPos + 1] );
+                ResultOfMultiply[arrayPos] -= Math.floor(ResultOfMultiply[arrayPos] / 10) * 10;
             }
         }
 
-        return ResultOfMultiply.reverse().join("");
+    }
+
+    return ResultOfMultiply.reverse().join("");
 
 };
